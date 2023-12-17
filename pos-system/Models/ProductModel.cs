393617efaa6f360f6fc22903363bb0845a14d4a4
shilingas <mariussilingas5@gmail.com>
2023-12-17 12:09:25 +0200

    added some models from data model

---
+++ pos-system/Models/ProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace pos_system.Models
+{
+    public class ProductModel
+    {
+        public string? Name { get; set; }
+        public float? Price { get; set; }
+        public string? Category { get; set; }
+    }
+}
---
+++ pos-system/Models/ServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace pos_system.Models
+{
+    public class ServiceModel
+    {
+        public string? Name { get; set; }
+        public float? Duration { get; set; }
+        public float? Price { get; set; }
+    }
+}
